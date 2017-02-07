baa9f6ebd039c5c78cb85f39f8aa6ffbd4a5092b
NICK VALLEE <nickvallee@gmail.com>
2017-02-07 10:16:35 -0500

    chore: run images through gulp responsive. images are larger than before. will test

--- gulp/tasks/responsive.js
+++ gulp/tasks/responsive.js
@@ -2,16 +2,24 @@ var gulp = require('gulp'),
 responsive = require('gulp-responsive');
 
 gulp.task('responsive', function () {
-  return gulp.src('./app/assets/img/*.{png,jpg}')
+  return gulp.src('./app/assets/views/images/*.{png,jpg}')
     .pipe(responsive({
       '*.png': {
         width: '100%',
-        quality: 20,
+        quality: 50,
         compressionLevel: 6,
         progressive: true,
-      withMetadata: false,
+        withMetadata: false,
         rename: { suffix: '-web' }
+      },
+      '*.jpg': {
+        width: '100%',
+        format: 'png',
+        rename: {
+          suffix: '-web',
+          extname: '.png'
       }
+    }
     }))
-    .pipe(gulp.dest('./app/assets/img/'));
+    .pipe(gulp.dest('./app/assets/views/images/'));
 });
